315d0e06612e855040f4aab1e2c4412b926441c7
niko@pcp18 <nikolai.zaki@bccn-berlin.de>
2018-04-09 15:56:32 +0200

    fixed edges

--- testing/maxima.py
+++ testing/maxima.py
@@ -1,4 +1,4 @@
-def find_maxima(x):
+def find_maxima(x,circular=False):
     """Find local maxima of x.
 
     Example:
@@ -8,14 +8,25 @@ def find_maxima(x):
 
     Input arguments:
         x -- 1D list numbers
-
+        circular - Bool, if True first and last are considered neighbours
     Output:
         idx -- list of indices of the local maxima in x
     """
 
-    idx = []
-    for i in range(len(x)):
-        # `i` is a local maximum if the signal decreases before and after it
-        if x[i-1] < x[i] and x[i+1] < x[i]:
-            idx.append(i)
+    if not circular:
+        idx = []
+
+        if x[0]>x[1]:
+            idx.append(0)
+
+        for i in range(1,len(x)-1):
+            # `i` is a local maximum if the signal decreases before and after it
+            if x[i-1] < x[i] and x[i+1] < x[i]:
+                idx.append(i)
+
+        if x[-1]>x[-2]:
+            idx.append(len(x))
+
+    if circular: idx = [i for i in range(len(x)) if x[i-1] < x[i] and x[(i+1)%len(x)] < x[i]]
+
     return idx
